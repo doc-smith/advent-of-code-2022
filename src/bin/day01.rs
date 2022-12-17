fae466d939b5ee80a7fc13752889937c0e53036a
Dmitry Kuznetsov <dimitrij.kuznetsov@gmail.com>
2022-12-17 21:00:58 +0100

    Refactored Day 1.

--- src/bin/day01.rs
+++ src/bin/day01.rs
@@ -1,54 +1,54 @@
 use std::cmp::Reverse;
-use std::collections;
+use std::collections::BinaryHeap;
 use std::io;
 
-fn read_supplies() -> Vec<u64> {
-    let mut supplies = Vec::new();
-
-    let mut elf = 0;
-
-    for line in io::stdin().lines() {
-        let l = String::from(line.expect("Failed to read line").trim());
-
-        if l.is_empty() {
-            supplies.push(elf);
-            elf = 0;
-        } else {
-            let calories: u64 = l.parse().expect("Failed to parse integer");
-            elf += calories;
-        }
-    }
-
-    if elf > 0 {
-        supplies.push(elf);
-    }
-
-    supplies
-}
-
-fn part_one(supplies: &[u64]) -> u64 {
-    *supplies.iter().max().unwrap()
-}
-
-fn top_k<T: Ord + Copy>(xs: &[T], k: usize) -> Vec<T> {
-    let mut heap = collections::BinaryHeap::from_iter(
-        xs.iter().take(k).map(|&x| Reverse(x)),
-    );
-    for &x in xs {
+fn top_k<T>(xs: &[T], k: usize) -> Vec<&T>
+where
+    T: Ord,
+{
+    let mut heap = BinaryHeap::from_iter(xs.iter().take(k).map(Reverse));
+    for x in xs.iter().skip(k) {
         heap.push(Reverse(x));
         if heap.len() > k {
             heap.pop();
         }
     }
-    heap.drain().map(|Reverse(x)| x).collect()
+    heap.into_iter().map(|Reverse(x)| x).collect()
 }
 
-fn part_two(supplies: &[u64]) -> u64 {
-    top_k(supplies, 3).iter().sum()
+fn read_input() -> Vec<u32> {
+    let mut elves = Vec::new();
+    let mut elf = 0;
+
+    for line in io::stdin().lines() {
+        let line = line.expect("input error");
+        if line.is_empty() {
+            elves.push(elf);
+            elf = 0;
+        } else {
+            elf += line
+                .parse::<u32>()
+                .expect("invalid input, expected an integer");
+        }
+    }
+
+    if elf > 0 {
+        elves.push(elf);
+    }
+
+    elves
+}
+
+fn part_one(elves: &[u32]) -> u32 {
+    *elves.iter().max().expect("empty input")
+}
+
+fn part_two(elves: &[u32]) -> u32 {
+    top_k(elves, 3).iter().copied().sum()
 }
 
 fn main() {
-    let supplies = read_supplies();
-    println!("{}", part_one(&supplies));
-    println!("{}", part_two(&supplies));
+    let elves = read_input();
+    println!("{}", part_one(&elves));
+    println!("{}", part_two(&elves));
 }
